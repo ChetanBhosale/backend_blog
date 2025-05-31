f83dbccf9726d33052ee47ddbcce7ba515ce763e
Suraj11000 <surajshindecs@gmail.com>
2025-05-31 23:25:07 +0530

    fix the pages section

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createBlog, getAdminBlogs, createContentUsingAI, deleteBlog, getBlogById, updateBlog, bannedUser, getDashboardAnalytics, getAllUser } = require("../controller/dashboard.controller");
+const { createBlog, getAdminBlogs, createContentUsingAI, deleteBlog, getBlogById, updateBlog, bannedUser, getDashboardAnalytics, getAllUser, getPageData, updatePageData } = require("../controller/dashboard.controller");
 const { uploadImage } = require("../middleware/uploadImage.middleware");
 const { authorizeRoles, authenticateUser } = require("../middleware/auth.middleware");
 const { route } = require("./blog.routes");
@@ -11,7 +11,8 @@ router.post("/create-content-using-ai",[authenticateUser,authorizeRoles('admin')
 router.delete('/delete-blog', [authenticateUser, authorizeRoles('admin')], deleteBlog)
 router.get('/get-blog/:id', getBlogById)
 router.put("/update-blog/:id", [authenticateUser, authorizeRoles('admin'), uploadImage], updateBlog);
-
+router.get('/pages', [authenticateUser, authorizeRoles('admin')], getPageData)
+router.put('/pages/:pageType', [authenticateUser, authorizeRoles('admin')], updatePageData)
 router.get('/users', [authenticateUser, authorizeRoles('admin')], getAllUser);
 router.post('/user-ban', [authenticateUser, authorizeRoles('admin')], bannedUser);
 
